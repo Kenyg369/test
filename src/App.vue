1f7f5763ce6eb9fd59c940a1c4643614ed2b4969
Kenyg369 <kengrand369@gmail.com>
2021-08-19 22:07:35 -0500

    first push

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorldInput 
+  v-model:firstName="firstName" 
+  v-model:lastName="lastName"  
+  />
+  
+  <span>My name is {{firstName + lastName}}</span>
+
+
+</template>
+
+<script setup>
+import {ref} from "vue"
+import HelloWorldInput from './components/HelloWorld.vue'
+
+const firstName = ref("firstName"); 
+const lastName = ref("lastName");
+
+
+</script>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
+
+/*   
+  v-model:firstname="firstName" 
+  v-model:lastname="lastName"  
+*/
+
+
+/* 
+  :firstName="firstName"
+  @update:firstName = "firstName = $event"
+  :lastName="lastName"
+  @update:lastName = "lastName = $event"
+ */
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,46 @@
+<template>
+  <p>
+    <a href="https://vitejs.dev/guide/features.html" target="_blank">
+      Vite Documentation
+    </a>
+    |
+    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
+  </p>
+
+  <button type="button" @click="state.count++">count is: {{ state.count }}</button>
+
+  <input type="text" placeholder="firstname" :value="firstName" @input="nameUpdate('firstName', $event.target.value)">
+  <input type="text" placeholder="lastname" :value="lastName" @input="nameUpdate('lastName', $event.target.value)">
+
+  <p>
+    Edit
+    <code>components/HelloWorld.vue</code> to test hot module replacement.
+  </p>
+</template>
+
+<script setup>
+import { reactive } from 'vue'
+const emit =defineEmits(["update:firstName","update:lastName"])
+const nameUpdate = (propName,val) => {emit(`update:${propName}`, val)} 
+
+const props = defineProps({
+  firstName: String,
+  lastName: String,
+})
+
+const state = reactive({ count: 0 })
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
+
+
+/* 
+<input type="text" placeholder="firstname" :value="firstName" @input="firstNameUpdate($event.target.value)">
+<input type="text" placeholder="lastname" :value="lastName" @input="lastNameUpdate($event.target.value)">
+
+const firstNameUpdate = (val) => {emit("update:firstName", val)} 
+const lastNameUpdate = (val) => {emit("update:lastName", val)}  */
